4cb1607da6aa690728257ac3c244f1301a12438d
Artem Prokhorov <electron3djavadev@gmail.com>
2022-10-04 14:13:14 +0300

    added alphabet classes

---
+++ src/Alphabet.java
@@ -0,0 +1,5 @@
+public abstract class Alphabet {
+    public int fromChar;
+    public int toChar;
+    public int numberOfLetters;
+}
---
+++ src/EnglishAlphabet.java
@@ -0,0 +1,3 @@
+public class EnglishAlphabet extends Alphabet {
+
+}
